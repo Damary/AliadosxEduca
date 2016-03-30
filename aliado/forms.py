818f652c5d2ff03544159e1b2dae5290b458b224
Damary <villatorodamary@gmail.com>
2016-03-30 15:05:07 -0600

    aish no funciona el login

--- aliado/forms.py
+++ aliado/forms.py
@@ -6,3 +6,7 @@ class AliadoFormulario(forms.ModelForm):
 	class Meta:
 		model = Aliado #toma todos los campos del modelo organizacion
 		exclude = ('usuario_creador', 'usuario_modifico')
+
+class LoginForm(forms.Form):
+	username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'usuario'}))
+	password = forms.CharField(widget=forms.PasswordInput(render_value=False, attrs={'placeholder': 'Password'}))	
--- aliadosxeducacion/urls.py
+++ aliadosxeducacion/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django.views.generic.base import TemplateView
+from django.contrib.auth.views import login
 
 urlpatterns = [
     # Examples:
@@ -9,9 +10,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name="login.html"), name="login"),
-	url(r'', include('aliado.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('aliado.urls')),
 
     
 ]
