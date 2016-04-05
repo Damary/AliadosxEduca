127150a92c17e549dfd7ee178018333614d02127
Damary <villatorodamary@gmail.com>
2016-04-05 14:58:10 -0600

    ppp

--- cuentas/forms.py
+++ cuentas/forms.py
@@ -12,6 +12,7 @@ class RegistroUserForm(forms.Form):
    
 
     def clean_username(self):
+        print 'clean_username'
         """Comprueba que no exista un username igual en la db"""
         username = self.cleaned_data['username']
         if User.objects.filter(username=username):
@@ -19,16 +20,22 @@ class RegistroUserForm(forms.Form):
         return username
 
     def clean_email(self):
+        print 'clean_email'
         """Comprueba que no exista un email igual en la db"""
         email = self.cleaned_data['email']
         if User.objects.filter(email=email):
             raise forms.ValidationError('Ya existe un email igual en la db.')
         return email
 
-    def clean_password2(self):
+    def clean_clave2(self):
+        print 'clean_password2'
         """Comprueba que password y password2 sean iguales."""
         clave = self.cleaned_data['clave']
         clave2 = self.cleaned_data['clave2']
         if clave != clave2:
+            print 'sadjasd', forms.ValidationError('Las contraseñas no coinciden.')
             raise forms.ValidationError('Las contraseñas no coinciden.')
+        else:
+            print '0000000000000000000000000000000'
+
         return clave2
--- cuentas/views.py
+++ cuentas/views.py
@@ -1,16 +1,52 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 
-# Create your views here.
-
-from django.shortcuts import render
 from .forms import RegistroUserForm
+from .models import PerfilUsuario
 
 
 def registro_usuario(request):
     if request.method == 'POST':
-        form = RegistroUserForm(request.POST)
+        # Si el method es post, obtenemos los datos del formulario
+        form = RegistroUserForm(request.POST, request.FILES)
+
+        # Comprobamos si el formulario es valido
+        if form.is_valid():
+            # En caso de ser valido, obtenemos los datos del formulario.
+            # form.cleaned_data obtiene los datos limpios y los pone en un
+            # diccionario con pares clave/valor, donde clave es el nombre del campo
+            # del formulario y el valor es el valor si existe.
+            cleaned_data = form.cleaned_data
+            usuario = cleaned_data.get('usuario')
+            clave = cleaned_data.get('clave')
+            email = cleaned_data.get('email')
+            
+            # E instanciamos un objeto User, con el username y password
+            user_model = User.objects.create_user(username=usuario, password=clave)
+            # Añadimos el email
+            user_model.email = email
+            # Y guardamos el objeto, esto guardara los datos en la db.
+            user_model.save()
+            # Ahora, creamos un objeto UserProfile, aunque no haya incluido
+            # una imagen, ya quedara la referencia creada en la db.
+            user_profile = UserProfile()
+            # Al campo user le asignamos el objeto user_model
+            user_profile.user = user_model
+            
+            # Por ultimo, guardamos tambien el objeto UserProfile
+            user_profile.save()
+            # Ahora, redireccionamos a la pagina cuentas/gracias.html
+            # Pero lo hacemos con un redirect.
+            print 'oiuewruierwre'
+            #return redirect(reverse('cuentas.gracias', kwargs={'username': username}))
     else:
+        # Si el mthod es GET, instanciamos un objeto RegistroUserForm vacio
+        print  'errorrrrrrrrrrrrrrrrrrrrrrrrrrrr'
         form = RegistroUserForm()
-    context = {'form': form }
+    # Creamos el contexto
+    context = {'form': form}
+    # Y mostramos los datos
     return render(request, 'registro.html', context)
